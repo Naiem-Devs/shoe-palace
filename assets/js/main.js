7fd9c2557ead616079f3bc92522a9c9fd4b3f753
Naiem-Devs <dnsnaiem@gmail.com>
2024-12-23 19:53:39 +0600

    page-add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,22 @@
-(function($) {
-  "use strict";
   
- // menu 
- $('.siteBar-btn').click( function (event){ 
-    event.preventDefault()
-    $(this).toggleClass('active');   
-    $('.mobile-menu').toggleClass('siteBar');   
-  }); 
+  // filter__sidebar Active()
+  var fltBtn = document.querySelector('.Filter__btn')
+  var fltClose = document.querySelector('.fltClose')
+  var fltSideBar = document.querySelector('.filter__sidebar')
+  var body = document.querySelector('body')
 
+  fltBtn.addEventListener('click', () => {
+    fltSideBar.classList.add('show')
+    body.classList.add('overflow-hidden')
+  })
 
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
-  });
+  fltClose.addEventListener('click', () => {
+    fltSideBar.classList.remove('show')
+    body.classList.remove('overflow-hidden')
+  })
+  
+  
 
+  
+  
 
-  // page Animation
-  // AOS.init({
-  //   mirror: true,
-  //   duration: 1500,
-  //   initClassName: 'aos-init',
-  //   once: true,
-  // });
-
-  // data-aos="fade-up" 
-  // data-aos-delay="300" 
-
- 
-})(jQuery);
